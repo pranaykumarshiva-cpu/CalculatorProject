004baa94cd0e3c345660a121b738bb9dd4c1557f
S.Pranay Kumar <pranaykumarshiva@gmail.com>
2026-08-25 10:51:57 +0530

    calculator program with addition feature

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,10 @@
 public class Calculator{
-  public int add(){
+  public int add(nt a,int b){
+	  int c=a+b;
+	  return c;
   }
   public static void main(String args[]){
+	  Calculator cal= new Calculator();
+	  System.out.println("The sum of two numbers is "+(cal.add(2,3)));
   }
 }
